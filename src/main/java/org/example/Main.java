ea63f98c19e06ac6caba87b6b1b07d515da0a827
Kirill Nikolaev <novoidx2012@gmail.com>
2022-04-26 17:36:42 +0300

    Create method1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Main {
+    private void method1() {
+        System.out.println("first method");
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
