e7925c6b57b03ffd9911823bc9a6c91a43d304f9
Kirill Nikolaev <novoidx2012@gmail.com>
2022-04-26 17:38:05 +0300

    Create first method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Main {
+    private void method1() {
+        System.out.println("this method is first");
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
